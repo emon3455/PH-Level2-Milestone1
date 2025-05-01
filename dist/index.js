87384477afa02a36e449996b1e10746cf1fd2e04
Emon <emon.mhk69@gmail.com>
2025-05-01 13:29:18 +0600

    initial setup done

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let name = "Emon";
+let age = 24;
+console.log(`My Name is: ${name}, I am ${age} yers old.`);
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+let name : string = "Emon";
+let age : number = 24
+console.log(`My Name is: ${name}, I am ${age} yers old.`);
+ 
