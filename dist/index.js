9f7a6aade1e24275e6ce0113bc73fffe7f7fdc58
Emon <emon.mhk69@gmail.com>
2025-05-01 13:59:48 +0600

    module 1 video 5 done

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,20 @@
 "use strict";
-let name = "Emon";
+// simple/premitive data types
+// string
+let fullName = "Emon";
+// number
 let age = 24;
-console.log(`My Name is: ${name}, I am ${age} yers old.`);
+// boolean
+let isAdmin = true;
+// null
+let x = null;
+// undefined
+let y = undefined;
+console.log(`My Name is: ${fullName}, I am ${age} yers old. He is a ${isAdmin ? "Admin" : "Not a Admin"}`);
+// non-premitive data type
+let friends = ["Emon", "Elina", "Sajjad", "Mahmudul", "Nabil"];
+friends.push("Mohiny");
+console.log("All Of My Frields:");
+friends.forEach(element => {
+    console.log(element);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,33 @@
-let name : string = "Emon";
+// simple/premitive data types
+
+// string
+let fullName : string = "Emon";
+// number
 let age : number = 24
-console.log(`My Name is: ${name}, I am ${age} yers old.`);
+// boolean
+let isAdmin:boolean = true
+// null
+let x: null = null
+// undefined
+let y: undefined = undefined
+
+console.log(`My Name is: ${fullName}, I am ${age} yers old. He is a ${isAdmin ? "Admin" : "Not a Admin"}`);
+
+// non-premitive data type
+let friends: string[] = ["Emon", "Elina", "Sajjad", "Mahmudul", "Nabil"]
+friends.push("Mohiny")
+
+console.log("All Of My Frields:");
+friends.forEach(element => {
+    console.log(element);
+});
+
+let friendsAge: number[] = [1,2,3,4,5,7]
+
+// tuple
+let person: [string,number,boolean] = ["Emon", 10, true]
+
+// Refere
+
+
  
