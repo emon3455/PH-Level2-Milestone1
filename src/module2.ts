e74cb95cac875df26a5f20a1432d326ed9f9db8a
Emon <emon.mhk69@gmail.com>
2025-05-03 22:45:56 +0600

    basic generic type done

--- src/module2.ts
+++ src/module2.ts
@@ -31,7 +31,31 @@
     interface user4 extends user2, user3 {}
 
 
-    
+    // Generic type
+
+    // for array
+    type GenericArray<T> = Array<T>
+
+    const rolleNumber: GenericArray<number> = [1,2,3,4,5,6];
+    const skills: GenericArray<string> = ["Javascript","TypeScript","Express"]
+
+    const manager: GenericArray<{name:string,salary: number}> = [
+        {
+            name:"Emon",
+            salary: 27000,
+        },
+        {
+            name:"Ariyan",
+            salary: 30000,
+        },
+    ]
+
+    // generic tuple
+    type genericTuple<x, y> = [x, y]
+
+    const human: genericTuple<string, string> = ["Emon", "Elina"]
+
+    const humanWithId: genericTuple<number, {name:string, age:number}> = [1234, {name:"Emon", age: 24}]
 
 
     // 
