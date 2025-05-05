9cdf011381a6339e746422450b9ebaa510c2fe58
emon3455 <emon.mhk69@gmail.com>
2025-05-05 12:20:50 +0600

    class and object done

--- src/module3.1.ts
+++ src/module3.1.ts
@@ -3,6 +3,19 @@
 
 
     // Class and Object
+    class Animal{
+
+        constructor(public name: string, public species: string, public sound: string){
+        }
+
+        makeSound(){
+            console.log(`${this.name} makes ${this.sound} sound.`);
+        }
+
+    }
+    const cat = new Animal("Cat", "Mammal", "Meow");
+
+    cat.makeSound()
     
 
 
