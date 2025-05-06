694868109022fea7ff4ecbb06ec72df7013502f5
Emon <emon.mhk69@gmail.com>
2025-05-07 01:03:21 +0600

    getter and setter done

--- src/module3.6.ts
+++ src/module3.6.ts
@@ -1,7 +1,35 @@
 {
-    // getter and setter
+  // getter and setter
 
-    
+  class BankAccount {
+    readonly id: number;
+    public userName: string;
+    protected _balance: number;
 
+    constructor(id: number, userName: string, _balance: number) {
+      this.id = id;
+      this.userName = userName;
+      this._balance = _balance;
+    }
 
+    get getBalance() {
+      return this._balance;
+    }
+
+    set depositBalance(balance: number) {
+      this._balance += balance;
+    }
+  }
+
+  const newUser = new BankAccount(1, "Mr. Boss", 500)
+
+//   calling getter
+  const oldBalance = newUser.getBalance
+  console.log("Old Balance: ", oldBalance);
+  
+//   calling setter
+  newUser.depositBalance = 1000
+
+  const newBalance = newUser.getBalance
+  console.log("New Balance: ", newBalance);
 }
