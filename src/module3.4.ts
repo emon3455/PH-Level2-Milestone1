7bc08ad04009148cd1ad95dfb28fcfa758f93d9e
emon3455 <emon.mhk69@gmail.com>
2025-05-06 14:45:28 +0600

    instance of done

--- src/module3.4.ts
+++ src/module3.4.ts
@@ -1,7 +1,64 @@
 {
-    // 
+    // instanceof Guard
+    class Animal{
+        name: string;
+        species: string;
 
-    
+        constructor(name: string, species: string){
+            this.name = name;
+            this.species = species;
+        }
+
+        makesound(){
+            console.log("Animal sound");
+        }
+
+    }
+
+    class Dog extends Animal{
+        constructor(name: string, species: string){
+            super(name, species);
+        }
+        makeBark(){
+            console.log("Bark Bark");
+        }
+    }
+
+    class Cat extends Animal{
+        constructor(name: string, species: string){
+            super(name, species);
+        }
+        makeMeow(){
+            console.log("Meow Meow");
+        }
+    }
+
+    const dog = new Dog("Mr. Dog", "Dog");
+    const cat = new Cat("Mr. Cat", "Cat");
+    const normalAnimal = new Animal("Mr. Normal", "Normal Animal");
+
+    const getDog = (animal: Animal): animal is Dog=>{
+        return animal instanceof Dog;
+    }
+    const getCat = (animal: Animal): animal is Cat =>{
+        return animal instanceof Cat;
+    }
+
+
+    const getAnimal = (animal: Animal)=>{
+        if(getDog(animal)){  
+            animal.makeBark();
+        }else if(getCat(animal)){
+            animal.makeMeow();      
+        }
+        else{
+            animal.makesound();
+        }
+    }
+
+    getAnimal(dog);
+    getAnimal(cat);
+    getAnimal(normalAnimal);
 
     // 
 }
