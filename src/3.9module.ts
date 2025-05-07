c40f2168ed94ebdd60c8ec25077040a4f67e0592
emon3455 <emon.mhk69@gmail.com>
2025-05-07 10:34:49 +0600

    abstraction done along with abstract class

--- src/3.9module.ts
+++ src/3.9module.ts
@@ -24,8 +24,35 @@
     }
 
     const newCar1 = new Car1()
-    newCar1.startEngine()   
-    newCar1.move()
-    newCar1.stopEngine()
+    // newCar1.startEngine()   
+    // newCar1.move()
+    // newCar1.stopEngine()
+
+
+    // Abstract using Abstract class
+
+    abstract class Vehicle2{
+        abstract startEngine(): void;
+        abstract stopEngine(): void;
+        abstract move(): void;
+
+    }
+
+    class Car2 extends Vehicle2{
+        startEngine(): void {
+            console.log("Car engine started");
+        }
+        stopEngine(): void {
+            console.log("Car engine stopped");
+        }
+        move(): void {
+            console.log("Car is moving");
+        }
+    }
+
+    const newCar2 = new Car2()
+    newCar2.startEngine()
+    newCar2.move()
+    newCar2.stopEngine()
 
 }
