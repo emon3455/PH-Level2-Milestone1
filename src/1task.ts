b0a542d54c646ae8b51c0aa23a3acf495721d7f9
Emon <emon.mhk69@gmail.com>
2025-05-07 21:25:45 +0600

    task 1 done

---
+++ src/1task.ts
@@ -0,0 +1,14 @@
+{
+    // problem-1
+    function formatString(input: string, toUpper?: boolean): string{
+        if(toUpper===false){
+            return input.toLowerCase();
+        }
+        else{
+            return input.toUpperCase();
+        }
+    }
+    console.log(formatString("Hello")); // Output: "HELLO"
+    console.log(formatString("Hello", true)); // Output: "HELLO"
+    console.log(formatString("Hello", false)); // Output: "hello"
+}
