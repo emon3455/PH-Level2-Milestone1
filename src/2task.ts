865ee7044e958be5b43f345bc4ed3d656e3a89c5
Emon <emon.mhk69@gmail.com>
2025-05-07 21:29:40 +0600

    task2 done

---
+++ src/2task.ts
@@ -0,0 +1,12 @@
+{
+    function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[]{
+        return items.filter((item: { title: string; rating: number })=> item.rating >=4)
+    }
+    const books = [
+        { title: "Book A", rating: 4.5 },
+        { title: "Book B", rating: 3.2 },
+        { title: "Book C", rating: 5.0 }
+      ];
+      
+    console.log(filterByRating(books));
+}
